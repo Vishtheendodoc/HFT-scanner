4fe735ad9ab90845cdfc4eac22f39d5a849f7ce9
Vishtheendodoc <dr.vishu.sp@gmail.com>
2025-11-04 11:55:15 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- nifty1.py
+++ nifty1.py
@@ -17,7 +17,7 @@ warnings.filterwarnings('ignore')
 IST = pytz.timezone("Asia/Kolkata")
 
 CLIENT_ID = '1100244268'
-ACCESS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzYyMjM2MzA3LCJpYXQiOjE3NjIxNDk5MDcsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTAwMjQ0MjY4In0.WyNM_jJI9g3o6r5fU96mmmA4352366Kt6eTrpSMtxuxRkdqs_2MMv9-0PPIZCasNR7BvCba-8k2Str7BH440Fw'
+ACCESS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzYyMzIzODM4LCJpYXQiOjE3NjIyMzc0MzgsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTAwMjQ0MjY4In0.nxmnFHQG2P51YDqiuJJE0HleeACIizEt2MVjYuQxZ_jDyYUTT-8tk-lO9zSG42OBnyGEB1__kvaA7szquS6F9g'
 
 HEADERS = {
     'client-id': CLIENT_ID,
@@ -567,6 +567,7 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
 
